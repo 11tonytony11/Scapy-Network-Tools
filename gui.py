e8f20e440898a578e134126667e4b4500db6d415
11tonytony11 <malin2122@gmail.com>
2020-07-10 16:28:23 +0300

    Improved UI and started initial work on connecting UI with backend

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QApplication, QTabWidget, QLineEdit
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QApplication, QTabWidget, QLineEdit, QGridLayout, QHBoxLayout, \
+    QStackedWidget
 
 
 class Window(QWidget):
@@ -9,44 +10,61 @@ class Window(QWidget):
 
         # Init window
         self.setWindowTitle("NET UTILS By TonyM")
+        self.resize(600, 300)
         layout = QVBoxLayout()
         self.setLayout(layout)
 
+        # Init Tabs
+        self.ping = Tab()
+        self.trace = Tab()
+        self.dns = Tab()
+
         # Init Tab Widget
-        tabwidget = QTabWidget()
-        tabwidget.addTab(Tab(), "Ping")
-        tabwidget.addTab(Tab(), "Traceroute")
-        tabwidget.addTab(Tab(), "NSlookup")
+        self.tabwidget = QTabWidget()
+        self.tabwidget.addTab(self.ping, "Ping")
+        self.tabwidget.addTab(self.trace, "Traceroute")
+        self.tabwidget.addTab(self.dns, "NSLookup")
 
         # Init exec button
         run = QtWidgets.QPushButton("Execute")
-        run.clicked.connect(lambda: update_gui(tabwidget.currentIndex()))
+        run.clicked.connect(lambda: update_gui(self.tabwidget.currentIndex(), self))
+        run.setFixedSize(100, 30)
 
         # Add all widgets to layout
-        layout.addWidget(tabwidget)
+        layout.addWidget(self.tabwidget)
         layout.addWidget(run)
 
 class Tab(QWidget):
     def __init__(self):
         super().__init__()
 
-        # Init Window
-        dst = QLabel("URL/IP:")
-        field = QLineEdit()
-
         # Init Layout
         layout = QVBoxLayout()
-        self.setLayout((layout))
+        self.setLayout(layout)
 
-        # Add Widgets to layout
-        layout.addWidget(dst)
-        layout.addWidget(field)
+        # Init Window
+        self.field = QLineEdit()
+        self.field.setPlaceholderText("Enter URL/IP")
+        self.tmp = QLabel("")  # Fix responsive issue
+
+        # Add widgets to layout
+        layout.addWidget(self.field, 0)
+        layout.addWidget(QLabel(""))
+        layout.addWidget(self.tmp)
 
 
-def update_gui(option):
-    print(option)
+def update_gui(option, obj):
+    if option == 0:
+        obj.ping.tmp.setText("Imagine UI is pinging")
+        # output = ping(obj.ping.field.text())
+    if option == 1:
+        obj.trace.tmp.setText("Imagine UI is tracing")
+        # output = trace(obj.ping.field.text())
+    if option == 2:
+        obj.dns.tmp.setText("Imagine UI is looking")
+        # output = nslookup(obj.ping.field.text())
 
-    # HOWTO Write: obj.layout().addWidget(QLabel("Hello DNS"))
+    #TODO: work with QListWidget ?
 
 
 if __name__ == "__main__":
